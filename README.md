9ae0304fefb2083ba9dafa0a9b94b0402d961e92
Sol Estravis <sole@bandofcoders.com>
2019-06-24 09:17:31 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Hours loader for Avalith
+
+## Installation instructions:
+Download and install [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en)
+on Chrome.
+
+Click on the Tampermonkey icon next to the URL bar, and select 
+```
+create a new script...
+```
+
+Paste the contents of script.js and have fun!
+
+### Customizable options:
+
+* Inspect your Project's select options to get the right project value.
+* There's some code commented out that will automatically turn work location to Home on Wednesdays.
---
+++ script.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         Hours loader for avalith
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  why type when you can have this script
+// @author       You
+// @match        http://horas.avalith.net/entry/add/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Select project:
+    const project = document.querySelector('select#id_project');
+    // Inspect element to get your project's value!
+    project.value = 0;
+
+    // Input activity:
+    const activity0 = document.querySelector('input#id_activity_0');
+    activity0.value = 'Development';
+    const activity1 = document.querySelector('input#id_activity_1');
+    activity1.value = '1'; // Development
+
+    // Select location:
+    const workLocation = document.querySelector('select#id_location');
+    workLocation.value = 1; // Office
+
+    // Optional code for Wednesdays home office
+    // if (new Date().getDay() === 3) { // Wednesdays
+    //     workLocation.value = 2; // Home
+    // } else { workLocation.value = 1; }
+
+    // Set todays Date:
+    const todaysDate = new Date().toJSON().slice(0, 10);
+    const startDate = document.querySelector('input#id_start_time_0');
+    const endDate = document.querySelector('input#id_end_time_0');
+    startDate.value = todaysDate;
+    endDate.value = todaysDate;
+
+    // Set working hours:
+    const startTime = document.querySelector('input#id_start_time_1');
+    const endTime = document.querySelector('input#id_end_time_1');
+    startTime.value = '9:00';
+    endTime.value = '18:00';
+
+    // Set break time:
+    const pauses = document.querySelector('input#id_seconds_paused');
+    pauses.value = '3600';
+
+    //Scroll down to description:
+    window.scroll({top: window.innerHeight, behavior: 'smooth'});
+
+    // Focus on description field:
+    const description = document.querySelector('textarea#id_comments');
+    description.focus();
+
+})();
